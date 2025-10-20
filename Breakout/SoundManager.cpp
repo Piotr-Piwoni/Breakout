2ef62e0351517f3f4fc7ddc925302a607c7f7d49
Piotr Piwoni <piotrpiwonibiz@gmail.com>
2025-10-21 00:31:28 +0100

    Created a basic SoundManager class with object pooling for active sounds and the ability to add & reference sounds by name.

---
+++ Breakout/SoundManager.cpp
@@ -0,0 +1,59 @@
+﻿#include "SoundManager.hpp"
+#include <iostream>
+
+SoundManager::SoundManager()
+{
+	m_ActiveSounds.reserve(30);
+}
+
+void SoundManager::Play(const std::string& name)
+{
+	const auto i = m_Sounds.find(name);
+	if (i == m_Sounds.end())
+		return;
+
+	const auto sound = std::make_shared<sf::Sound>();
+	sound->setBuffer(*i->second);
+	sound->play();
+
+	m_ActiveSounds.push_back({name, sound});
+}
+
+void SoundManager::StopAll() const
+{
+	for (auto& sound : m_ActiveSounds)
+		sound.Sound->stop();
+}
+
+void SoundManager::LoadFile(const std::string& name, const std::string& path)
+{
+	const auto buffer = std::make_shared<sf::SoundBuffer>();
+	if (!buffer->loadFromFile(path))
+	{
+		std::cerr << "Failed to load sound! Path: " << path << '\n';
+		return;
+	}
+
+	m_Sounds[name] = buffer;
+}
+
+bool SoundManager::IsPlaying(const std::string& name) const
+{
+	for (const auto& [Name, Sound] : m_ActiveSounds)
+		if (Name == name && Sound->getStatus() == sf::Sound::Playing)
+			return true;
+
+	return false;
+}
+
+void SoundManager::Update()
+{
+	// Removed sounds that finished playing.
+	for (auto it = m_ActiveSounds.begin(); it != m_ActiveSounds.end();)
+	{
+		if (it->Sound->getStatus() == sf::Sound::Stopped)
+			it = m_ActiveSounds.erase(it);
+		else
+			++it;
+	}
+}
---
+++ Breakout/SoundManager.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <map>
+#include "SFML/Audio.hpp"
+
+struct ActiveSound
+{
+	std::string Name;
+	std::shared_ptr<sf::Sound> Sound;
+};
+
+
+class SoundManager
+{
+public:
+	SoundManager();
+
+	void Play(const std::string& name);
+	void StopAll() const;
+
+	void LoadFile(const std::string& name, const std::string& path);
+
+	bool IsPlaying(const std::string& name) const;
+
+	void Update();
+
+private:
+	std::map<std::string, std::shared_ptr<sf::SoundBuffer>> m_Sounds{};
+	std::vector<ActiveSound> m_ActiveSounds{};
+};
